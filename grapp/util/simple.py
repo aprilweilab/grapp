7ca7c59378f2475f9ac6e0942ce05545e8b57c5a
Drew DeHaas <drewdehaas@gmail.com>
2025-09-15 15:09:18 -0400

    Add missing data support to allele_frequencies
    Pass "adjust_missing=True" to calculate using missing data information.

--- grapp/util/simple.py
+++ grapp/util/simple.py
@@ -10,18 +10,31 @@ class UserInputError(Exception):
     pass
 
 
-def allele_frequencies(grg: pygrgl.GRG) -> numpy.typing.NDArray:
+def allele_frequencies(
+    grg: pygrgl.GRG, adjust_missing: bool = False
+) -> numpy.typing.NDArray:
     """
     Get the allele frequencies for the mutations in the given GRG.
 
     :param grg: The GRG.
     :type grg: pygrgl.GRG
+    :param adjust_missing: Optional. Set to true to adjust each allele frequncies to be
+        :math:`\\frac{count_i}{total - missing_i}` instead of :math:`\\frac{count_i}{total}`.
+    :type adjust_missing: bool
     :return: A vector of length grg.num_mutations, containing allele frequencies
         indexed by MutationID.
     :rtype: numpy.ndarray
     """
-    return pygrgl.matmul(
+    if adjust_missing:
+        miss = numpy.zeros(grg.num_mutations, dtype=numpy.int32)
+    else:
+        miss = None
+    counts = pygrgl.matmul(
         grg,
         numpy.ones((1, grg.num_samples), dtype=numpy.int32),
         pygrgl.TraversalDirection.UP,
-    )[0] / (grg.num_samples)
+        miss=miss,
+    )[0]
+    if miss is None:
+        miss = 0
+    return counts / (grg.num_samples - miss)
