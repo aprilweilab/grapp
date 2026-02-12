42b790e7d888675185ab127a5a0025b001502d09
Drew DeHaas <drewdehaas@gmail.com>
2026-02-12 10:57:42 -0500

    Command line: "grapp assoc" support for multiple GRGs (#32)
    Convenient for when you have multiple chromosomes. Since this is
    just simple linear regression, they can be done independently, but
    this way they can be done in parallel (`-j 10`, e.g. for 10 threads)

--- grapp/cli/assoc_cli.py
+++ grapp/cli/assoc_cli.py
@@ -5,7 +5,9 @@ from grapp.assoc import (
     linear_assoc_no_covar,
 )
 from grapp.cli.util import pandas_to_tsv
+from typing import Optional
 import argparse
+import concurrent.futures
 import numpy
 import os
 import pandas
@@ -13,7 +15,11 @@ import pygrgl
 
 
 def add_options(subparser):
-    subparser.add_argument("grg_input", help="The input GRG file")
+    subparser.add_argument(
+        "grg_input",
+        nargs="+",
+        help="One or more input GRG file. If multiple are provided, the GWAS can be run in parallel (see --jobs)",
+    )
     subparser.add_argument(
         "-p",
         "--phenotypes",
@@ -42,18 +48,39 @@ def add_options(subparser):
         action="store_true",
         help="Standardize the X and Y matrices prior to performing linear regression.",
     )
+    subparser.add_argument(
+        "-j",
+        "--jobs",
+        type=int,
+        help="Use this many threads when processing more than one GRG.",
+    )
+
+
+def do_single_gwas(
+    grg: pygrgl.GRG,
+    y: numpy.typing.NDArray,
+    C: Optional[numpy.typing.NDArray],
+    method: Optional[str],
+    standardize: bool,
+):
+    if C is not None:
+        assert method is not None
+        gwas_df = linear_assoc_covar(grg, y, C, method=method, standardize=standardize)
+    else:
+        gwas_df = linear_assoc_no_covar(grg, y, standardize=standardize)
+    return gwas_df
 
 
 def run(args):
-    g = pygrgl.load_immutable_grg(args.grg_input, load_up_edges=False)
+    grgs = [pygrgl.load_immutable_grg(g, load_up_edges=False) for g in args.grg_input]
     if args.phenotypes is None:
         print("No phenotype provided; randomly generating phenotype values")
-        y = numpy.random.standard_normal(g.num_individuals)
+        y = numpy.random.standard_normal(grgs[0].num_individuals)
     else:
         y = read_pheno(args.phenotypes)
         assert (
-            len(y) == g.num_individuals
-        ), f"Phenotype file had {len(y)} rows, expected {g.num_individuals}"
+            len(y) == grgs[0].num_individuals
+        ), f"Phenotype file had {len(y)} rows, expected {grgs[0].num_individuals}"
 
     if args.covariates is not None:
         if args.covariates.endswith(".txt"):
@@ -64,18 +91,36 @@ def run(args):
             ), "Covariates filename must end in .txt (plink format) or .tsv (pandas dataframe)"
             C = pandas.read_csv(args.covariates, delimiter="\t").to_numpy()
         method = "regress" if args.regress_y else "QR"
-        gwas_df = linear_assoc_covar(
-            g, y, C, method=method, standardize=args.standardize
-        )
     else:
         assert not args.regress_y, "--regress-y doesn't apply to non-covariate GWAS"
-        gwas_df = linear_assoc_no_covar(g, y, standardize=args.standardize)
+        method = None
+        C = None
 
-    if args.out_file is None:
-        args.out_file = f"{os.path.basename(args.grg_input)}.assoc.tsv"
-
-    pandas_to_tsv(args.out_file, gwas_df)
-    print(f"Wrote results to {args.out_file}")
+    if len(grgs) > 1:
+        executor = concurrent.futures.ThreadPoolExecutor(max_workers=args.jobs)
+        futures = [
+            executor.submit(do_single_gwas, g, y, C, method, args.standardize)
+            for g in grgs
+        ]
+        dataframes = [f.result() for f in futures]
+        if args.out_file is None:
+            for gf, df in zip(args.grg_input, dataframes):
+                out_file = f"{os.path.basename(gf)}.assoc.tsv"
+                pandas_to_tsv(out_file, df)
+                print(f"Wrote results to {out_file}")
+        else:
+            # Add a column that indicates which file each result came from, and concat
+            for gf, df in zip(args.grg_input, dataframes):
+                df["GRG"] = [os.path.basename(gf)] * len(df)
+            df = pandas.concat(dataframes)
+            pandas_to_tsv(args.out_file, df)
+            print(f"Wrote results to {args.out_file}")
+    else:
+        gwas_df = do_single_gwas(grgs[0], y, C, method, args.standardize)
+        if args.out_file is None:
+            args.out_file = f"{os.path.basename(args.grg_input[0])}.assoc.tsv"
+        pandas_to_tsv(args.out_file, gwas_df)
+        print(f"Wrote results to {args.out_file}")
 
 
 if __name__ == "__main__":
