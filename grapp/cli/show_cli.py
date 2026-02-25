d2dbe1fc0d42625811b89b1c0569347f1d956912
Drew DeHaas <drewdehaas@gmail.com>
2026-02-25 11:42:36 -0500

    More mutation-based filtering from the command line (#37)
    Emulate bcftools with -m/-M and -v (--types). This lets us filter
    to bi-allelic SNPs, e.g.
    
    Add `grapp show -c` and `grapp show -a` for allele counts and
    allele frequencies, respectively.
    
    Also, added a cheatsheet for commands.

--- grapp/cli/show_cli.py
+++ grapp/cli/show_cli.py
@@ -1,6 +1,13 @@
 import argparse
+import numpy
+import pandas
 import pygrgl
+import sys
 from collections import defaultdict
+from grapp.util.simple import (
+    allele_counts,
+    allele_frequencies,
+)
 
 
 def add_options(subparser: argparse.ArgumentParser):
@@ -18,16 +25,35 @@ def add_options(subparser: argparse.ArgumentParser):
         action="store_true",
         help="Show the population information.",
     )
+    whattoshow.add_argument(
+        "-i",
+        "--info",
+        action="store_true",
+        help="Show the high-level GRG information.",
+    )
+    whattoshow.add_argument(
+        "-f",
+        "--frequencies",
+        action="store_true",
+        help="Show the tab-separated allele frequency information.",
+    )
+    whattoshow.add_argument(
+        "-c",
+        "--counts",
+        action="store_true",
+        help="Show the tab-separated allele counts information.",
+    )
 
 
 def run(args):
     grg = pygrgl.load_immutable_grg(args.grg_input, load_up_edges=False)
-    print(f"Mutations: {grg.num_mutations}")
-    print(f"Samples: {grg.num_samples}")
-    print(f"Individuals: {grg.num_individuals}")
-    print(f"Phased? {'Yes' if grg.is_phased else 'No'}")
-    print(f"Nodes: {grg.num_nodes}")
-    print(f"Edges: {grg.num_edges}")
+    if args.info:
+        print(f"Mutations: {grg.num_mutations}")
+        print(f"Samples: {grg.num_samples}")
+        print(f"Individuals: {grg.num_individuals}")
+        print(f"Phased? {'Yes' if grg.is_phased else 'No'}")
+        print(f"Nodes: {grg.num_nodes}")
+        print(f"Edges: {grg.num_edges}")
     if args.individuals:
         if grg.has_individual_ids:
             print("\n".join(map(grg.get_individual_id, range(grg.num_individuals))))
@@ -39,3 +65,29 @@ def run(args):
                 by_pop[grg.get_population_id(sample_id)] += 1
             for p, label in enumerate(pop_labels):
                 print(f"{label}: {by_pop[p]}/{grg.num_samples} haplotypes")
+    if args.frequencies:
+        freq = allele_frequencies(grg, adjust_missing=True)
+        muts = list(map(lambda m: grg.get_mutation_by_id(m), range(grg.num_mutations)))
+        df = pandas.DataFrame(
+            {
+                "Position": numpy.array(map(lambda m: m.position, muts)),
+                "REF": numpy.array(map(lambda m: m.ref_allele, muts)),
+                "ALT": numpy.array(map(lambda m: m.allele, muts)),
+                "Frequency": freq,
+            }
+        )
+        df.to_csv(sys.stdout, sep="\t", index=False)
+    if args.counts:
+        acount, amiss = allele_counts(grg, return_missing=True)
+        muts = list(map(lambda m: grg.get_mutation_by_id(m), range(grg.num_mutations)))
+        df = pandas.DataFrame(
+            {
+                "Position": numpy.array(map(lambda m: m.position, muts)),
+                "REF": numpy.array(map(lambda m: m.ref_allele, muts)),
+                "ALT": numpy.array(map(lambda m: m.allele, muts)),
+                "ALT Count": acount,
+                "Missing Count": amiss,
+                "Total": grg.num_samples,
+            }
+        )
+        df.to_csv(sys.stdout, sep="\t", index=False)
